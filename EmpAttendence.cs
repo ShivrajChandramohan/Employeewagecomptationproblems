9e9262ce936d8438520c687c904cb02951b1f334
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-24 21:47:15 +0530

    Employee Check

---
+++ EmpAttendence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonStaticEmpWageProblem
+{
+    public class EmpAttendence
+    {
+        public void EmployeeCheck()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
